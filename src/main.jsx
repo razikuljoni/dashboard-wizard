cd404e5d1e36444aaac2f2657a64f595c292c5c8
MD Razikul Islam Joni <razikuljoni@gmail.com>
2026-05-16 01:10:36 +0600

    feat: add redux-persist, react-google-charts, and react-icons dependencies while removing Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -11,7 +10,6 @@ createRoot(document.getElementById("root")).render(
         <BrowserRouter>
             <Providers>
                 <App />
-                <Analytics />
             </Providers>
         </BrowserRouter>
     </StrictMode>
